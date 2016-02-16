82eb23b033ce750250a07a0846425cb57cad9f85
ElisDN <mail@elisdn.ru>
2016-02-16 23:06:23 +0300

    Added tests environment

---
+++ tests/AuthManagerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace elisdn\hybrid\tests;
+
+class AuthManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
